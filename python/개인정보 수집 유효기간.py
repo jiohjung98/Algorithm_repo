04c5127fef12d2027d2f3fdfb11b23baa7517e19
jiohjung98 <ckaquz98@naver.com>
2024-03-22 18:20:27 +0900

    개인정보 수집 유효기간

--- python/개인정보 수집 유효기간.py
+++ python/개인정보 수집 유효기간.py
@@ -1,30 +1,23 @@
-def parse_date(date_str):
-    return tuple(map(int, date_str.split('.')))
-
-def compare_dates(date1, date2):
-    return date1 <= date2
-
 def solution(today, terms, privacies):
-    today = parse_date(today)
     answer = []
-
-    for index, pr in enumerate(privacies, 1):
-        pr_date = parse_date(pr[:10])
+    for i, pr in enumerate(privacies, 1):
         for t in terms:
             if t.split()[0] in pr:
-                year_plus = int(t.split()[1]) // 12
-                month_plus = int(t.split()[1]) % 12
+                year_plus = (int(t.split()[1])) // 12
+                month_plus = (int(t.split()[1])) % 12
                 new_year = int(pr[0:4]) + year_plus
-                new_month = pr_date[1] + month_plus
+                new_month = int(pr[5:7]) + month_plus   
                 if new_month > 12:
                     new_year += 1
                     new_month -= 12
-                new_date = (new_year, new_month, pr_date[2])
-                if compare_dates(new_date, today):
-                    answer.append(index)
+                if len(str(new_month)) == 1:
+                    new_month = '0' + str(new_month)
+                new_date = str(new_year) + '.' + str(new_month) + '.' + pr[8:10]
+                print(new_date)
+                if new_date <= today:
+                    answer.append(i)
                     break
     return answer
-
 
 today = "2022.05.19"
 terms = ["A 6", "B 12", "C 3"]
