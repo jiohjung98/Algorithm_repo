5123cf43e5cd0dda6e4b5088d071af667d115af7
jiohjung98 <ckaquz98@naver.com>
2024-11-05 18:22:10 +0900

    swea - 비밀번호

---
+++ python/swea - 비밀번호.py
@@ -0,0 +1,23 @@
+for tc in range(1, 11):
+    n, m = input().split()
+    arr = list(m)
+    stack = []
+    
+    for i in arr:
+        if len(stack) == 0:
+            stack.append(i)
+        else:
+            if stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+    
+    for elem in stack:
+        elem = int(elem)
+    ans = ''.join(stack)
+
+    print(f'#{tc} {ans}')
+
+    # print(f'{tc}',' ',*stack, sep='')
+
+   
