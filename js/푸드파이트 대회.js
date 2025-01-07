bfe255b0c7faf964a36babfdd24e0d85d31476a8
jiohjung98 <ckaquz98@naver.com>
2025-01-07 16:34:55 +0900

    js: 푸드파이트 대회(repeat() 사용)

--- js/푸드파이트 대회.js
+++ js/푸드파이트 대회.js
@@ -2,9 +2,12 @@ function solution(food) {
     var arr = [];
     for (let i=1; i<food.length; i++) {
         let cnt = Math.floor(food[i] / 2);
-        for (let j=0; j<cnt; j++) {
-            arr.push(i);
-        }
+        // for (let j=0; j<cnt; j++) {
+        //     arr.push(i);
+        // }
+
+        // for문 대신 .repeat() 사용
+        arr.push(String(i).repeat(cnt));
     }
     // 주의사항: 그냥 arr.reverse()를 하게되면 arr자체가 뒤집어짐 -> 원본배열이 수정됨
     // reverse()의 영향을 받지 않기 위해 arr 배열을 복사한 뒤 사용해야 함!!(전개연산자 사용)
