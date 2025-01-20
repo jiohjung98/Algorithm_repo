1d79293128f522598d94e87b5a7f7e5bde3cce11
jiohjung98 <ckaquz98@naver.com>
2025-01-20 18:29:35 +0900

    js: 숫자의 표현

--- js/숫자의 표현.js
+++ js/숫자의 표현.js
@@ -1,16 +1,29 @@
+// function solution(n) {
+//     var answer = 0;
+//     for (let i = 1; i <= n; i++) { 
+//         var sum = 0;
+//         for (let j = i; j <= n; j++) { 
+//             sum += j;
+//             if (sum === n) {
+//                 answer++;
+//                 break;
+//             } else if (sum > n) {
+//                 break;
+//             }
+//         }
+//     }
+//     return answer;
+// }
+
 function solution(n) {
-    var answer = 0;
-    for (let i = 1; i <= n; i++) { 
-        var sum = 0;
-        for (let j = i; j <= n; j++) { 
-            sum += j;
-            if (sum === n) {
-                answer++;
-                break;
-            } else if (sum > n) {
-                break;
-            }
+    let answer = 0;
+
+    for (let m = 1; m * (m - 1) / 2 < n; m++) {
+        // n - m * (m - 1) / 2가 m으로 나누어떨어지면 가능
+        if ((n - (m * (m - 1)) / 2) % m === 0) {
+            answer++;
         }
     }
+
     return answer;
 }
