c0cddb50b1ca974766df9f9613dfa0dbf463a641
jiohjung98 <ckaquz98@naver.com>
2025-01-28 16:44:27 +0900

    py: 타겟 넘버(dfs, bfs)

---
+++ python/타겟 넘버(dfs, bfs).py
@@ -0,0 +1,29 @@
+# bfs
+def solution(numbers, target):
+    leaves = [0]
+    for number in numbers:
+        tmp = []
+        for leaf in leaves:
+            tmp.append(leaf + number)
+            tmp.append(leaf - number)
+        leaves = tmp
+    answer = leaves.count(target)
+    return answer
+
+# dfs
+answer = 0
+
+def dfs(numbers, target, current, index):
+    global answer
+    if len(numbers) == index:
+        if current == target:
+            answer += 1
+        return
+    
+    dfs(numbers, target, current + numbers[index], index + 1)
+    dfs(numbers, target, current - numbers[index], index + 1)
+
+
+def solution(numbers, target):
+    dfs(numbers, target, 0, 0)
+    return answer
