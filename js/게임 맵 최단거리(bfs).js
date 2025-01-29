93353625a8e3ad8c455c9db4103398ca172b26c8
jiohjung98 <ckaquz98@naver.com>
2025-01-29 14:03:33 +0900

    js: 게임 맵 최단거리(bfs)

---
+++ js/게임 맵 최단거리(bfs).js
@@ -0,0 +1,26 @@
+function solution(maps) {
+    const dx = [-1,1,0,0];
+    const dy = [0,0,-1,1];
+    
+    const queue = [[0,0]];
+    
+    while (queue.length) {
+        const [x,y] = queue.shift();
+        
+        for (let i=0; i<4; i++) {
+            var nx = x + dx[i];
+            var ny = y + dy[i];
+            
+            if (nx < 0 || nx >= maps.length || ny < 0 || ny >= maps[0].length) continue;
+            if (maps[nx][ny] === 0) continue;
+            if (maps[nx][ny] === 1) {
+                maps[nx][ny] = maps[x][y] + 1
+                queue.push([nx,ny]);
+            }
+        }
+    }
+    
+    const answer = maps[maps.length-1][maps[0].length-1];
+    
+    return answer === 1 ? -1 : answer;
+}
