fa141557ce4325f977e8173310785c8d04f67257
jiohjung98 <ckaquz98@naver.com>
2025-02-04 15:23:58 +0900

    RGB거리(1149 - dp)

---
+++ js/RGB거리(1149).js
@@ -0,0 +1,11 @@
+N = int(input())
+dp = [[0] * 3 for _ in range(N)]
+rgb_arr = [list(map(int, input().split())) for _ in range(N)]
+
+dp[0] = rgb_arr[0]
+for i in range(1, N):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgb_arr[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + rgb_arr[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + rgb_arr[i][2]
+
+print(min(dp[-1]))
