3dea18ecd56d5c1865d04c54a4b6c41a5b5be326
jiohjung98 <ckaquz98@naver.com>
2025-02-04 20:44:09 +0900

    계단오르기(2579 - dp)

--- js/계단오르기(2579).js
+++ js/계단오르기(2579).js
@@ -1,16 +1,20 @@
-n = int(input())
-stairs = [0] + [int(input()) for _ in range(n)]
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
+const N = Number(input[0]);
+const stairs = [0];
+for (let n=1; n <= N; n++) {
+    a = Number(input[n]);
+    stairs.push(a);
+}
 
-if n == 1:
-    print(stairs[1])
-elif n == 2:
-    print(stairs[1] + stairs[2])
-else:
-    dp = [0] * (n+1)
-    dp[1] = stairs[1]
-    dp[2] = stairs[1] + stairs[2]
-    for i in range(3, n+1):
-        dp[i] = max(dp[i-2], dp[i-3] + stairs[i-1]) + stairs[i]
-
-    print(dp[n])
+if (N === 1) console.log(stairs[1]);
+else if (N === 2) console.log(stairs[1] + stairs[2]);
+else {
+    const dp = Array.from({length: N+1}, () => 0);
+    dp[1] = stairs[1];
+    dp[2] = dp[1] + stairs[2];
+    for (let i=3; i<=N; i++) {
+        dp[i] = Math.max(dp[i-2], dp[i-3] + stairs[i-1]) + stairs[i];
+    }
+    console.log(dp[N]);
+}
