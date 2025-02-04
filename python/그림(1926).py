70162091e64dd8067edd9e81d2d21ac9609d0466
jiohjung98 <ckaquz98@naver.com>
2025-02-04 23:55:49 +0900

    py: 그림(1926 - bfs)

---
+++ python/그림(1926).py
@@ -0,0 +1,50 @@
+from collections import deque
+
+n, m = tuple(map(int, input().split()))
+arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+extent_arr = []
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    visited[x][y] = True
+    global extent
+    extent = 1
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x +dx[i]
+            ny = y +dy[i]
+
+            if nx >= 0 and nx < n and ny >= 0 and ny < m and arr[nx][ny] == 1 and visited[nx][ny] == False:
+                visited[nx][ny] = True
+                arr[nx][ny] = arr[x][y] + 1
+                extent += 1
+                queue.append((nx,ny))
+    extent_arr.append(extent)
+    
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 1:
+            bfs(i,j)
+            cnt += 1
+
+print(cnt)
+print(max(extent_arr))
+
+
+
+# [[1, 2, 0, 1, 2],
+#  [0, 3, 4, 0, 0],
+#  [0, 0, 0, 0, 0],
+#  [1, 0, 1, 2, 3],
+#  [0, 0, 2, 3, 4],
+#  [0, 0, 3, 4, 5]] 4
--- python/피보나치 수 2(2748).py
+++ python/피보나치 수 2(2748).py
@@ -1,8 +1,8 @@
 n = int(input())
 
-if n == 1:
+if n == 0:
     print(0)
-elif n == 2:
+elif n == 1:
     print(1)
 else:
     dp = [0] * (n+1)
