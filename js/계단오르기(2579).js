86cbfd49987a20b4adec27bbceb3dc74a8da6554
jiohjung98 <ckaquz98@naver.com>
2025-02-04 20:36:43 +0900

    계단오르기(2579 - dp)

---
+++ js/계단오르기(2579).js
@@ -0,0 +1,16 @@
+n = int(input())
+stairs = [0] + [int(input()) for _ in range(n)]
+
+
+if n == 1:
+    print(stairs[1])
+elif n == 2:
+    print(stairs[1] + stairs[2])
+else:
+    dp = [0] * (n+1)
+    dp[1] = stairs[1]
+    dp[2] = stairs[1] + stairs[2]
+    for i in range(3, n+1):
+        dp[i] = max(dp[i-2], dp[i-3] + stairs[i-1]) + stairs[i]
+
+    print(dp[n])
