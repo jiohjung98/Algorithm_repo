6f4057347285fe4f89b42c37a96e391d99eb5ee9
jiohjung98 <ckaquz98@naver.com>
2025-02-08 00:40:42 +0900

    softeer - 출퇴근길(bfs, dfs)

--- python/출퇴근길.py
+++ python/출퇴근길.py
@@ -47,6 +47,7 @@
 
 
 
+# 방법 2 - bfs
 # import sys
 # input = sys.stdin.readline
 # sys.setrecursionlimit(10**6)
@@ -94,7 +95,7 @@
 # print(cnt-2)
 
 
-# 방법 2 - bfs
+
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -119,8 +120,8 @@ def bfs(node, graph, visited):
         now = queue.popleft()
         for neighbor in graph[now]:
             if not visited[neighbor]:
-                queue.append(neighbor)
                 visited[neighbor] = 1
+                queue.append(neighbor)
 
 visited_s_to_t = [0] * (n+1)
 visited_s_to_t[t] = 1 # 마지막 도착지 1로 미리 설정
@@ -131,14 +132,17 @@ visited_t_to_s[s] = 1 # 마지막 도착지 1로 미리 설정
 bfs(t, graph, visited_t_to_s) 
 
 visited_s_to_t_reverse = [0] * (n+1)
-bfs(s, graph, visited_s_to_t_reverse)
+bfs(s, graph_reverse, visited_s_to_t_reverse)
 
 visited_t_to_s_reverse = [0] * (n+1)
-bfs(t, graph, visited_t_to_s_reverse) 
+bfs(t, graph_reverse, visited_t_to_s_reverse) 
 
 cnt = 0
 for i in range(1, n+1):
     if visited_s_to_t[i] and visited_t_to_s[i] and visited_s_to_t_reverse[i] and visited_t_to_s_reverse[i]:
         cnt += 1
 
-print(cnt-2)
+if cnt > 2:
+    print(cnt-2)
+else:
+    print(0)
