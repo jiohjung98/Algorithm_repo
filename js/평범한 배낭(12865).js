701aef67d3889504f2605869ad2def9035b7687b
jiohjung98 <ckaquz98@naver.com>
2025-02-10 21:09:00 +0900

    구간 합 구하기 5(11660 - dp)

--- js/평범한 배낭(12865).js
+++ js/평범한 배낭(12865).js
@@ -1,18 +1,24 @@
-n, k = map(int, input().split())
-bag = [(0,0)]
-for _ in range(n):
-    w, v = map(int, input().split())
-    bag.append((w,v))
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-dp = [[0] * (k+1) for _ in range(n+1)]
+const [n, k] = input[0].split(' ').map(Number);
+const bag = [[0,0]];
 
-for i in range(1, n+1):
-    w, v = bag[i]
-    # j = 현재 수용 가능한 무게
-    for j in range(1, k+1):
-        if j < w:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
+for (let i=1; i<=n; i++) {
+    const [w, v] = input[i].split(' ').map(Number);
+    bag.push([w,v]);
+}
 
-print(dp[n][k])
+const dp = Array.from({length: n+1}, () => Array(k+1).fill(0));
+
+for (let i=1; i<=n; i++) {
+    const [w, v] = bag[i];
+    for (let j=1; j<=k; j++) {
+        if (j < w) {
+            dp[i][j] = dp[i-1][j];
+        } else {
+            dp[i][j] = Math.max(dp[i-1][j-w] + v, dp[i-1][j]);
+        }
+    }
+}
+
+console.log(dp[n][k]);
