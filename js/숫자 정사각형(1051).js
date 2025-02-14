a170ae953508c718a79954d9466105fb424b4f69
jiohjung98 <ckaquz98@naver.com>
2025-02-14 16:49:06 +0900

    js: 숫자 정사각형(1051)

--- js/숫자 정사각형(1051).js
+++ js/숫자 정사각형(1051).js
@@ -5,3 +5,25 @@ const arr = [];
 for (let i=1; i<=n; i++) {
     arr.push(input[i].split('').map(Number));
 }
+
+function findSquare(s) {
+    for (let i=0; i<n-s+1; i++) {
+        for (let j=0; j<m-s+1; j++) {
+            if (arr[i][j] === arr[i + s - 1][j] &&
+                arr[i][j] === arr[i + s - 1][j + s - 1] &&
+                arr[i][j] === arr[i][j + s - 1]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+const minValue = Math.min(n, m);
+
+for (let x = minValue; x >= 0; x--) {
+    if (findSquare(x)) {
+        console.log(x**2);
+        break;
+    }
+}
