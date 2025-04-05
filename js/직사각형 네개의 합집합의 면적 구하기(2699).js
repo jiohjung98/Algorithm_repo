d7dd9ea9c01ff1be2e7e9fd68b50c5b39a1c1f6f
jiohjung98 <ckaquz98@naver.com>
2025-04-05 20:03:32 +0900

    직사각형 네게의 합집합의 면적 구하기(2669)

---
+++ js/직사각형 네개의 합집합의 면적 구하기(2699).js
@@ -0,0 +1,18 @@
+let matrix = Array.from({length:100}, () => Array(100).fill(0));
+
+for (let i=0; i<4; i++) {
+    let [x1, y1, x2, y2] = input[i].split(' ').map(Number);
+    for (let a=y1; a<y2; a++) {
+        for (let b=x1; b<x2; b++) {
+            matrix[a][b] = 1;
+        }
+    }
+}
+
+let result = 0;
+
+for (let col of matrix) {
+    result += col.reduce((acc,cur) => acc+cur, 0);
+}
+
+console.log(result);
