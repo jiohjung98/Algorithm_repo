bec495c291ad1929c9c49af40ce7b4a0645f27b8
jiohjung98 <ckaquz98@naver.com>
2025-05-01 21:38:00 +0900

    py: 설탕배달(2839)

--- js/그림(1926) copy2.js
+++ js/그림(1926) copy2.js
@@ -1,47 +1,49 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-n, m =  tuple(map(int, input().split()))
-graph = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+const [n, m] = input[0].split(' ').map(Number);
+const graph = input.slice(1, n+1).map((line) => line.split(' ').map(Number));
+const visited = Array.from({length:n}, () => Array(m).fill(false));
 
-visited = [[False] * m for _ in range(n)]
+const dx = [-1,1,0,0];
+const dy = [0,0,-1,1];
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-cnt_list = []
+let cnt_list = [];
 
-def bfs(x,y):
-    queue = deque()
-    queue.append((x,y))
-    visited[x][y] = True
-    cnt = 1
+function bfs(x,y) {
+    let queue = [];
+    queue.push([x,y]);
+    visited[x][y] = true;
+    let cnt = 1;
 
-    while queue:
-        x,y = queue.popleft()
+    while (queue.length) {
+        let [x,y] = queue.shift();
 
-        for i in range(4):
-            nx =  x + dx[i]
-            ny =  y + dy[i]
+        for (let i=0; i<4; i++) {
+            let nx = x + dx[i];
+            let ny = y + dy[i];
 
-            if nx >= 0 and nx < n and ny >= 0 and ny < m and not visited[nx][ny] and graph[nx][ny] == 1:
-                queue.append((nx,ny))
-                visited[nx][ny] = True
-                cnt += 1
-    cnt_list.append(cnt)
+            if (nx >=0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && graph[nx][ny] === 1) {
+                queue.push([nx,ny]);
+                visited[nx][ny] = true;
+                cnt++;
+            }
+        }
+    }
+    cnt_list.push(cnt);
+}
 
-for i in range(n):
-    for j in range(m):
-        if not visited[i][j] and graph[i][j] == 1:
-            bfs(i,j)
+for (let i=0; i<n; i++) {
+    for (let j=0; j<m; j++) {
+        if (!visited[i][j] && graph[i][j] === 1) {
+            bfs(i,j);
+        }
+    }
+}
 
-
-if len(cnt_list) > 0:
-    print(len(cnt_list))
-    print(max(cnt_list))
-else:
-    print(0)
-    print(0)
+if (cnt_list.length > 0) {
+    console.log(cnt_list.length);
+    console.log(Math.max(...cnt_list));
+} else {
+    console.log(0);
+    console.log(0);
+}
