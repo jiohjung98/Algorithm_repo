d4f705b4c5e8b3cd9eb39c9e566fc0b22bf6266f
jiohjung98 <ckaquz98@naver.com>
2025-05-04 21:55:23 +0900

    dp: 설탕배달(2839).js

--- js/설탕배달(2839).js
+++ js/설탕배달(2839).js
@@ -1,13 +1,16 @@
-sugar = int(input())
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-bag = 0
-while sugar >= 0:
-    if sugar % 5 == 0:
-        bag += (sugar // 5)
-        print(bag)
-        break
-    sugar -= 3
-    bag += 1
-else:
-    print(-1)
-    
+const sugar = Number(input[0]);
+const dp = Array.from({length: sugar+1}, () => Infinity);
+dp[0] = 0;
+
+for (let i=3; i<sugar+1; i++) {
+    if (dp[i-3] !== Infinity) {
+        dp[i] = Math.min(dp[i], dp[i-3]+1);
+    }
+    if (i >= 5 && dp[i-5] !== Infinity) {
+        dp[i] = Math.min(dp[i], dp[i-5]+1);
+    }
+}
+
+console.log(dp[sugar] !== Infinity ? dp[sugar] : -1);
