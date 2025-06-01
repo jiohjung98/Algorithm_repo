5aead9206824b2f1e64e752d2242b0f013221719
jiohjung98 <ckaquz98@naver.com>
2025-06-01 18:26:14 +0900

    js: 연산자 끼워넣기((a, b) => ~~(a / b) 사용))

--- js/연산자 끼워넣기(14888).js
+++ js/연산자 끼워넣기(14888).js
@@ -7,7 +7,7 @@ const operation = input[2].split(' ').map(Number);
 let maxValue = -1e9;
 let minValue = 1e9;
 
-function bfs(depth, total, plus, minus, multiply, divide) {
+function dfs(depth, total, plus, minus, multiply, divide) {
     if (depth === n) {
         maxValue = Math.max(maxValue, total);
         minValue = Math.min(minValue, total);
@@ -15,19 +15,19 @@ function bfs(depth, total, plus, minus, multiply, divide) {
     }
 
     if (plus) {
-        bfs(depth+1, total+nums[depth], plus-1, minus, multiply, divide);
+        dfs(depth+1, total+nums[depth], plus-1, minus, multiply, divide);
     }
     if (minus) {
-        bfs(depth+1, total-nums[depth], plus, minus-1, multiply, divide);
+        dfs(depth+1, total-nums[depth], plus, minus-1, multiply, divide);
     }
     if (multiply) {
-        bfs(depth+1, total*nums[depth], plus, minus, multiply-1, divide);
+        dfs(depth+1, total*nums[depth], plus, minus, multiply-1, divide);
     }
     if (divide) {
-        bfs(depth+1, parseInt(total/nums[depth]), plus, minus, multiply, divide-1);
+        dfs(depth+1, ~~(total/nums[depth]), plus, minus, multiply, divide-1);
     }
 }
 
-bfs(1, nums[0], operation[0], operation[1], operation[2], operation[3]);
+dfs(1, nums[0], operation[0], operation[1], operation[2], operation[3]);
 console.log(maxValue);
 console.log(minValue);
