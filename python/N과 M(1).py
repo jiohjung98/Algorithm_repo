f9fac3d943ce0691f38ebd3592a79c9af04d62b7
jiohjung98 <ckaquz98@naver.com>
2025-06-06 15:06:30 +0900

    py: N과 M(1) - backtracking

---
+++ python/N과 M(1).py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = tuple(map(int, input().split()))
+answer = []
+
+def backtracking():
+    if len(answer) == m:
+       print(' '.join(map(str, answer)))
+       return
+    
+    for i in range(1,n+1):
+        if i not in answer:
+            answer.append(i)
+            backtracking()
+            answer.pop()
+
+backtracking()
